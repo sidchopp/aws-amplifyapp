9f283334f3c829bcf85f4c0df844779a9805378b
Sid <sidchopp@gmail.com>
2022-02-11 14:13:53 -0700

    added auth

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
-
+import React from 'react';
 import './App.css';
+import Authentication from './components/Authentication';
 
 function App() {
   return (
     <div className="App">
       <h1>Hello World! </h1>
       <p>This is my first ever AWS deployment. Hurrayy!!</p>
+      <h3>We have Authentication in our App.</h3>
+      <Authentication />
     </div>
   );
 }
