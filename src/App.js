01a4b90a04b96fef92afa865f4043076e8de092a
Sid <sidchopp@gmail.com>
2022-02-12 13:46:56 -0700

    GraphQL, bucket created with permissions and App is fully working

--- src/App.js
+++ src/App.js
@@ -2,17 +2,19 @@ import React from 'react';
 import './App.css';
 import { Authenticator } from '@aws-amplify/ui-react';
 import '@aws-amplify/ui-react/styles.css';
+import Api from './components/Api';
 
 function App() {
   return (
     <div className="App">
+      <h3>This is my first ever AWS deployment. Hurrayy!!!</h3>
       <Authenticator loginMechanisms={['username']}>
         {({ signOut, user }) => (
           <main>
-            <h3>This is my first ever AWS deployment. Hurrayy!!!</h3>
-            <h1>Hello {user.username}</h1>
-            <p> Authentication is working, if you are seeing this message.</p>
-            <button onClick={signOut}>Sign out</button>
+            <h1>Hello {user.username}
+              <span>  <button onClick={signOut}>Sign out</button></span>
+            </h1>
+            <Api />
           </main>
         )}
       </Authenticator>
